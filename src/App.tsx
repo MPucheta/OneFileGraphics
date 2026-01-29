26798360a985c1bf5af36e573ebf4a61a03dbee4
MPucheta <smpuchetadev@gmail.com>
2026-01-29 02:22:16 -0300

    try-fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ import Dashboard from './components/Dashboard';
 import RouteMap from './constants/routes';
 
 function App () {
+  const basename = import.meta.env.PROD
+    ? new URL(import.meta.env.BASE_URL).pathname
+    : '';
+
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL} >
+    <BrowserRouter basename={basename} >
       <Routes>
         <Route path={RouteMap.DASHBOARD} element={<Dashboard />} />
         <Route path={RouteMap.F_MATRIX_PERSPECTIVE} element={<FSample />} />
